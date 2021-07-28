447f763d8de6f79b654325cb6d4def8c50732e9c
zjw <newzjw713@163.com>
2021-07-28 22:36:56 +0800

    自定义axios

---
+++ node-study/21_axios/test.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: your name
+ * @Date: 2021-07-28 00:43:27
+ * @LastEditTime: 2021-07-28 00:43:43
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \node-study\21_axios\test.js
+ */
+import axios from 'axios'
+console.log(axios)
